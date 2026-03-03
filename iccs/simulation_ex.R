9fe5b70f82cec9d04d8979bd316768b59c3ea995
Aaron Matthew Simmons <45770852+ammsimmons@users.noreply.github.com>
2026-03-03 01:01:20 -0600

    custom sim driver

--- iccs/simulation_ex.R
+++ iccs/simulation_ex.R
@@ -45,41 +45,10 @@ params <- expand_grid( !!!design_factors) |>
 # #index SEED
 # SEED <- params$SEED 
 
-# #one condition test
-# target_icc <- TARGET_ICCS[1]
-# n_raters <- RATERS[3]
-# n_objects <- OBJECTS[3]
-# p <- PROPORTION[1]
-
-
-# n_raters <- 24
-# n_objects <- 100
-# target_icc <- 0.5
-# p <- 0.5
-# iter <- 10
-
-# RUN BINARY SIMULATIONS (simhelper doesn't work with f_analyze )
-# Binary_sim <- bundle_sim(
-#   f_generate = run_one_binary,
-#   f_analyze = calc_icc
-# )
-
-
-# run_all_binary <- function(
-#   ITERATIONS, DIR,icc_target,
-#   RATERS[3],OBJECTS[3], PROPORTION[1]){
-
-#   furrr::future_pwalk(P, run_one_cond, iter =iter, 
-#     dir = dir, .progress = TRUE,
-#   .options = furrr::furrr_options(seed = TRUE))
-#   #disable the error 
-
-#   }
-
 tictoc::tic()
 future::plan(multicore, workers = 6)
 #future::plan(sequential)
-sim_results <- vardel::run_all_binary(params, iter, writeFiles=TRUE)
+sim_results <- vardel::run_all_binary(params, iter, writeFiles=FALSE)
 tictoc::toc()
 
 # # saving results requires a new passed filename
