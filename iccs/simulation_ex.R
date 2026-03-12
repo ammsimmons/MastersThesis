0cb44273ad80ccf556f95a9c28aebb0a401777ee
Aaron Matthew Simmons <45770852+ammsimmons@users.noreply.github.com>
2026-03-12 15:14:35 -0500

    ordinal_sim run1

--- iccs/simulation_ex.R
+++ iccs/simulation_ex.R
@@ -1,4 +1,4 @@
-#' Main Simulation Script
+#' Main Binary Simulation Script
 #' @description
 #' 
 #' Goal: Run Simulations 
@@ -27,10 +27,10 @@ DIR <- "iccs/data"
 design_factors <- list(
 n_raters = c(3,12,24),
 n_objects = c(10,50,100),
-target_icc = c(0.50,0.60,0.70),
-p = c(0.5, 0.9)# for the binary case
+target_icc = c(0.40,0.60,0.80),
+p = c(0.5, 0.8)# for the binary case
 ) 
-iter <- 10
+iter <- 1000
 # params <- expand_grid( !!!design_factors) %>%
 #   mutate(
 #     SEED = 02112026 + 17 * 1:n() #set seed for each row 
