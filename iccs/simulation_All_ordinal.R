4fbc450cb4316b0e7c6f07b39537369efe4910d1
Aaron Matthew Simmons <45770852+ammsimmons@users.noreply.github.com>
2026-04-27 10:51:39 -0500

    Sim_Universe #2: ready to run

--- iccs/simulation_All_ordinal.R
+++ iccs/simulation_All_ordinal.R
@@ -31,22 +31,31 @@ target_icc = c(0.40,0.60,0.80),
 k_category = c(3,5,7),
 e_category = c(TRUE,FALSE) # 1 = equal category prevalence, #0 = unequal linear decay prevalence 
 ) 
-iter <- 1000
+iter <- 2
 
 
-params <- expand_grid( !!!design_factors) |>
+param <- expand_grid( !!!design_factors) |>
     mutate(
-    seed = 03122026 + 17 * 1:n(), #set seed for each row
-    filename = paste0("iccs/data/",seed,".rds")
+    seed = 03122026 + 17 * 1:n(), #set seed for each row,
+    condition = 1:n() * 1,
+    filename = paste0("iccs/data/ordinal_",condition,"_",seed,".rds")
   )
 
 
-# run sim
+# run sim (in portions)
+params_comp1 <- param |>
+  filter(condition <= 81) # for fast server 1 
+
+params_comp2 <- param |> 
+  filter(condition <= 82) # for fast server 1 
+
+
+
 
 tictoc::tic()
 future::plan(multisession, workers = 6)
 #future::plan(sequential)
-sim_results <- vardel::run_all_ordinal(params, iter, writeFiles=FALSE)
+sim_results <- vardel::run_all_ordinal(params_comp1, iter, writeFiles=TRUE)
 tictoc::toc()
 
 
