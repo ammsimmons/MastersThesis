fdd08e1c806f3a273900d8e92130ee160fe52086
Aaron Matthew Simmons <45770852+ammsimmons@users.noreply.github.com>
2026-04-30 22:15:48 -0500

    Sim_Universe2: All Binary ready (+ ordinal aov run)

--- iccs/simulation_All_Binary.R
+++ iccs/simulation_All_Binary.R
@@ -46,25 +46,8 @@ params <- expand_grid( !!!design_factors) |>
 # SEED <- params$SEED 
 
 tictoc::tic()
-future::plan(multicore, workers = 6)
+future::plan(multisession, workers = 22)
 #future::plan(sequential)
-sim_results <- vardel::run_all_binary(params, iter, writeFiles=FALSE)
+sim_results <- vardel::run_all_binary(params, iter, writeFiles=TRUE)
 tictoc::toc()
-
-# # saving results requires a new passed filename
-# params_saved <- params |>
-#   mutate(
-#     filename = paste0("iccs/data/",seed,".rds")
-#   )
-
-# saved_estim_icc <- function(..., filename = NA_character_){
-#   res <- safely(vardel::run_all_binary)(...)
-
-#   saveRDS(res$result$res, file = filename)
-   
-#   #return(res$error)
-#    }
-
-
-#sim_results <- pmap(params_mod, Pearson_sim, reps = 1000 )
 
