3010ed8734d183ae0e41f94b258158065195c1fa
Aaron Simmons <amsimmons@ku.edu>
2026-05-03 23:40:16 -0500

    Ordinal(U3) first 162 conditions

--- iccs/simulation_All_ordinal.R
+++ iccs/simulation_All_ordinal.R
@@ -38,34 +38,36 @@ param <- expand_grid( !!!design_factors) |>
     mutate(
     seed = 03122026 + 17 * 1:n(), #set seed for each row,
     condition = 1:n() * 1,
-    filename = paste0("iccs/data/ordinal_",condition,"_",seed,".rds")
+    filename = paste0("iccs/data/ordinal_UThree_",condition,"_",seed,".rds")
   )
 
 ##### Selecting Conditions 
 
-# run sim (in portions)
-#params_comp1 <- param |>
- # filter(condition <= 2) # for fast server 1 
+#run sim (in portions)
+params_comp1 <- param |>
+ filter(condition <= 162) # for fast server 1 
 
-#params_comp2 <- param |> 
- # filter(condition <= 82) # for fast server 1 
+# params_comp2 <- param |> 
+#  filter(condition <= 82) # for fast server 1 
 
-# Find errored conditions and rerun 
-# 1. List all .rds files in the directory
-files <- list.files(path = "~/MastersThesis/iccs/data/", pattern = "\\.rds$", full.names = FALSE)
+############ CODE FOR RUNNING SPECIFIC CONDITIONS 
+
+# # Find errored conditions and rerun 
+# # 1. List all .rds files in the directory
+# files <- list.files(path = "~/MastersThesis/iccs/data/", pattern = "\\.rds$", full.names = FALSE)
   
-#2. Extract the number from the middle of the filename
-# This regex looks for a sequence of digits (\d+)
-file_numbers <- as.numeric(stringr::str_extract(files, "\\d+")) 
-`%notin%` <- Negate(`%in%`)  
-filt_param <- param |> filter(condition %notin% file_numbers)
+# #2. Extract the number from the middle of the filename
+# # This regex looks for a sequence of digits (\d+)
+# file_numbers <- as.numeric(stringr::str_extract(files, "\\d+")) 
+# `%notin%` <- Negate(`%in%`)  
+# filt_param <- param |> filter(condition %notin% file_numbers)
 
 #####################
 
 tictoc::tic()
 future::plan(multisession, workers = 22)
 #future::plan(sequential)
-sim_results <- vardel::run_all_ordinal(filt_param, iter, writeFiles=TRUE)
+sim_results <- vardel::run_all_ordinal(params_comp1, iter, writeFiles=TRUE)
 tictoc::toc()
 
 
