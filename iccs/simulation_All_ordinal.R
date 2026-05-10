a9d012b436aa097bb9991249d721abde75eca237
Aaron Simmons <amsimmons@ku.edu>
2026-05-10 11:13:21 -0500

    U3 ordinal glmmonly almost done

--- iccs/simulation_All_ordinal.R
+++ iccs/simulation_All_ordinal.R
@@ -52,20 +52,20 @@ param <- expand_grid( !!!design_factors) |>
 
 # Find errored conditions and rerun 
 # 1. List all .rds files in the directory
-# files <- list.files(path = "~/MastersThesis/iccs/data/", pattern = "\\.rds$", full.names = FALSE)
+files <- list.files(path = "~/MastersThesis/iccs/data/", pattern = "\\.rds$", full.names = FALSE)
   
-# #2. Extract the number from the middle of the filename
-# # This regex looks for a sequence of digits (\d+)
-# file_numbers <- as.numeric(stringr::str_extract(files, "\\d+")) 
-# `%notin%` <- Negate(`%in%`)  
-# filt_param <- param |> filter(condition %notin% file_numbers)
+#2. Extract the number from the middle of the filename
+# This regex looks for a sequence of digits (\d+)
+file_numbers <- as.numeric(stringr::str_extract(files, "\\d+")) 
+`%notin%` <- Negate(`%in%`)  
+filt_param <- param |> filter(condition %notin% file_numbers)
 
 #####################
 
 tictoc::tic()
 future::plan(multisession, workers = 22)
 #future::plan(sequential)
-sim_results <- vardel::run_all_ordinal_onlyglmmtmb(param, iter, writeFiles=TRUE)
+sim_results <- vardel::run_all_ordinal_onlyglmmtmb(filt_param, iter, writeFiles=TRUE)
 tictoc::toc()
 
 
